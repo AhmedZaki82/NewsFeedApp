dc79d018513bc7c2f0d22e0224d67174418fc28f
Tsultrim <born.2p.great@gmail.com>
2018-06-21 21:37:31 +0200

    Stage2

--- app/src/main/java/com/example/android/newsfeedapp/MainActivity.java
+++ app/src/main/java/com/example/android/newsfeedapp/MainActivity.java
@@ -4,12 +4,16 @@ import android.app.LoaderManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -23,13 +27,31 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity
         implements LoaderManager.LoaderCallbacks<List<NewsFeed>>{
 
+    private static final String GUARDIAN_REQUEST_URL =
+            "http://content.guardianapis.com/search?";
+
+    // Constant for the API search Key
+    private static final String API_KEY = "api-key";
+
+    // Constant value for the API Key
+    private static final String KEY = "f1dfc1ea-9071-49cc-b586-005ed71ac92c";
+
+    // Constant value for ordering by date
+    private static final String ORDER = "order-by";
+    private static final String DATE = "newest";
+
+    // Constant value for No's of articles
+    private static final String PAGE = "page-size";
+    private static final String PAGES = "20";
+
+    // Constant value for section
+    private static final String SECTION = "section";
+
     private static final int NEWS_LOADER_ID = 1;
 
     private NewsAdapter mAdapter;
 
     public static final String LOG_TAG = MainActivity.class.getName();
-
-    private static final String GUARDIAN_REQUEST_URL = "https://content.guardianapis.com/search?from-date=2018-06-10&to-date=2018-06-11&use-date=published&order-by=oldest&api-key=test";
 
     private TextView mEmptyStateTextView;
 
@@ -86,14 +108,46 @@ public class MainActivity extends AppCompatActivity
                 startActivity(websiteIntent);
             }
         });
-
     }
 
     @Override
     public Loader<List<NewsFeed>> onCreateLoader(int i, Bundle bundle) {
 
-        Log.i(LOG_TAG,"Test: onCreateLoader is Called.....");
-        return new NewsLoader(this,GUARDIAN_REQUEST_URL);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String searchCategory = sharedPreferences.getString(
+                getString(R.string.pick_category1),
+                getString(R.string.all));
+
+        //extract news category name from list menu
+        String[] categorySep = searchCategory.split(" ");
+        String categoryCap = categorySep[0];
+        String category = categoryCap.toLowerCase();
+
+        // Create an URI and an URI Builder
+        Uri baseUri = Uri.parse(GUARDIAN_REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        //for all news category tag is not aplly
+        if (!category.equals("all")) {
+            uriBuilder.appendQueryParameter(SECTION, category);
+        }
+
+        // for US new change tag
+        if (category.equals(getString(R.string.tagUS))) {
+            category = category + getString(R.string.tagForUsUk);
+            uriBuilder.appendQueryParameter(SECTION, category);
+        }
+        // for UK new change tag
+        if (category.equals(getString(R.string.tagUK))) {
+            category = category + getString(R.string.tagForUsUk);
+            uriBuilder.appendQueryParameter(SECTION, category);
+        }
+
+        uriBuilder.appendQueryParameter(ORDER, DATE);
+        uriBuilder.appendQueryParameter(PAGE, PAGES);
+        uriBuilder.appendQueryParameter(API_KEY, KEY);
+
+        return new NewsLoader(this, uriBuilder.toString());
     }
 
     @Override
@@ -115,5 +169,24 @@ public class MainActivity extends AppCompatActivity
 
         Log.i(LOG_TAG,"Test: onLoaderReset is called.....");
         mAdapter.clear();
+    }
+
+    @Override
+    // This method initialize the contents of the Activity's options menu.
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the Options Menu we specified in XML
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
